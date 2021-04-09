e50d71d42af565a0a515b036e166aa8987347cd9
yeonheelee <mrncd1@gmail.com>
2021-04-09 14:50:10 +0900

    exclusive, nonexclusive example

---
+++ exclusive/simple_block_2_writer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include "simple_block_2.h"
+
+void main(void){
+	int fd, ret, cmd;
+	long value;
+
+	printf("choose cmd: ");
+	scanf("%d", &cmd);
+	printf("Input value: ");
+	scanf("%ld", &value);
+
+	fd = open("/dev/simple_block_2_dev",O_RDWR);
+	switch(cmd){
+		case 1:
+			ret = ioctl(fd, WQ_WAKE_UP, (unsigned long) value);
+			break;
+		case 2:
+			ret = ioctl(fd, WQ_WAKE_UP_ALL, (unsigned long) value);
+			break;
+	}
+	printf("success top produce: %d\n",ret);
+	close(fd);
+}
